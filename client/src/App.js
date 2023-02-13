fc749e789891969a6cfc9d628b708008e2f89466
Mudassar Hussain <ushasri6838@gmail.com>
2023-02-13 23:06:51 +0500

    add images and contant section

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import react, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import img from './Assest/img.jpg'
 
 function App() {
 
@@ -35,7 +36,15 @@ function App() {
 
 
   return (
-    <div className="w-50 p-3">
+    <div className='p-2'>
+      <div className='d-flex'>
+        <img src={img} alt="..." className="img-thumbnail " style={{width:'300px',height:'300px',objectFit:'cover'}} />
+        <div className='w-75 contant'>
+          <h3>James Smith</h3>
+          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+        </div>
+    </div>
+    <div className="w-50 p-3 mt-4">
       <div className="d-flex">
         <label  className="form-label w-50">Enter First Number</label>
         <input type="number" className="form-control" id="Number1" name='num1' value={number.num1} onChange={HandleNumberInput}></input>
@@ -51,6 +60,7 @@ function App() {
         <h4>Your Addition Result(from server) is: <span>{result}</span></h4>
         <h4>Your Addition Result(from client) is: <span>{SResult}</span></h4>
       </div>
+    </div>
     </div>
   );
 }
